df11e433e10b9b35442139aa00901f8ef634179e
7vanael <kristi.n.norris777@gmail.com>
2025-06-12 12:57:34 -0700

    Command line arguments appear good

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,17 @@
 package org.example;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Main {
     public static int port = 80;
     public static String root = ".";
 
-    private static String usage = "-p     Specify the port.  Default is 80.\n" +
-            "-r     Specify the root directory.  Default is the current working directory.\n" +
-            "-h     Print this help message\n" +
-            "-x     Print the startup configuration without starting the server\n";
+    private static String usage = "  -p     Specify the port.  Default is 80.\n" +
+            "  -r     Specify the root directory.  Default is the current working directory.\n" +
+            "  -h     Print this help message\n" +
+            "  -x     Print the startup configuration without starting the server\n";
 
     public static void main(String[] args) {
         if (parseArgs(args)) {
@@ -20,22 +22,27 @@ public class Main {
 
     private static boolean parseArgs(String[] args) {
         boolean helpFlag = false;
-        boolean blockFlag = false;
+        boolean printFlag = false;
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-p":
+                    int targetP;
                     if (i + 1 >= args.length) {
                         System.out.println(usage);
                         return false;
                     } else {
                         try {
-                            port = Integer.parseInt(args[i + 1]);
+                            targetP = Integer.parseInt(args[i + 1]);
                             i++;
+                            if(isValidPort(targetP)){
+                                port = targetP;
+                            }
                         } catch (NumberFormatException e) {
                             System.out.println("Invalid port");
                             System.out.println(usage);
                             return false;
                         }
+
                     }
                     break;
                 case "-r":
@@ -43,8 +50,13 @@ public class Main {
                         System.out.println(usage);
                         return false;
                     } else {
-                        root = args[i + 1];
+                        String targetR = args[i + 1];
                         i++;
+                        if(isValidRoot(targetR)){
+                            root = targetR;
+                        }else {
+                            return false;
+                        }
                     }
                     break;
                 case "-h":
@@ -52,11 +64,7 @@ public class Main {
                     helpFlag = true;
                     break;
                 case "-x":
-                    String absolutePath = new File(root).getAbsolutePath();
-                    System.out.println("Challenge Server");
-                    System.out.println("Running on port: " + port);
-                    System.out.println("Serving files from: " + absolutePath);
-                    blockFlag = true;
+                    printFlag = true;
                     break;
                 default:
                     System.out.println("Option not known: " + args[i]);
@@ -64,6 +72,42 @@ public class Main {
                     return false;
             }
         }
-        return !helpFlag && !blockFlag;
+        if(printFlag){
+            String absolutePath = new File(root).getAbsolutePath();
+            System.out.println("Challenge Server");
+            System.out.println("Running on port: " + port);
+            System.out.println("Serving files from: " + absolutePath);
+        }
+        return !helpFlag && !printFlag;
+    }
+
+    private static boolean isValidRoot(String targetR) {
+        if(targetR.isEmpty()){
+            System.out.println("Root path cannot be empty.");
+            System.out.println(usage);
+            return false;
+        }
+        File rootDir = new File(targetR);
+        if(!rootDir.exists()){
+            System.out.println("Directory does not exist: " + targetR);
+            return false;
+        }else if (!rootDir.isDirectory()){
+            System.out.println("Not a directory: " + targetR);
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isValidPort(int targetP) {
+        if(targetP < 1 || 65535 < targetP){
+            System.out.println("Port must be between 1 and 65535");
+            return false;
+        }
+        try(ServerSocket socket = new ServerSocket(targetP)){
+            return true;
+        }catch (IOException e){
+            System.out.println("Port already in use: " + targetP);
+            return false;
+        }
     }
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,11 +1,14 @@
 import org.example.Main;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.net.ServerSocket;
 
 import static org.junit.Assert.assertTrue;
 
@@ -29,19 +32,19 @@ public class ServerTest {
     public void printsUsageWithoutStartingServer() {
         Main.main(new String[] {"-h"});
         String output = outContent.toString();
-        assertTrue(output.contains("-p     Specify the port.  Default is 80."));
-        assertTrue(output.contains("-r     Specify the root directory.  Default is the current working directory."));
-        assertTrue(output.contains("-h     Print this help message"));
-        assertTrue(output.contains("-x     Print the startup configuration without starting the server"));
+        Assertions.assertTrue(output.contains("-p     Specify the port.  Default is 80."));
+        Assertions.assertTrue(output.contains("-r     Specify the root directory.  Default is the current working directory."));
+        Assertions.assertTrue(output.contains("-h     Print this help message"));
+        Assertions.assertTrue(output.contains("-x     Print the startup configuration without starting the server"));
     }
 
     @Test
     public void printsConfigWithoutStartingServer() {
         Main.main(new String[] {"-x"});
         String output = outContent.toString();
-        assertTrue(output.contains("Challenge Server"));
-        assertTrue(output.contains("Running on port: 80"));
-        assertTrue(output.contains("Serving files from: " + new File(".").getAbsolutePath()));
+        Assertions.assertTrue(output.contains("Challenge Server"));
+        Assertions.assertTrue(output.contains("Running on port: 80"));
+        Assertions.assertTrue(output.contains("Serving files from: " + new File(".").getAbsolutePath()));
     }
 
     @Test
@@ -49,7 +52,7 @@ public class ServerTest {
         Main.main(new String[]{"-x"});
         String output = outContent.toString();
         String expected = "Running on port: 80";
-        assertTrue(output.contains(expected));
+        Assertions.assertTrue(output.contains(expected));
     }
 
     @Test
@@ -57,7 +60,7 @@ public class ServerTest {
         Main.main(new String[]{"-x"});
         String output = outContent.toString();
         String expected = "Serving files from: " + new File(".").getAbsolutePath();
-        assertTrue(output.contains(expected));
+        Assertions.assertTrue(output.contains(expected));
     }
 
     @Test
@@ -65,7 +68,7 @@ public class ServerTest {
         Main.main(new String[]{"-p", "200", "-x"});
         String output = outContent.toString();
         String expected = "Running on port: 200";
-        assertTrue(output.contains(expected));
+        Assertions.assertTrue(output.contains(expected));
     }
 
     @Test
@@ -73,6 +76,76 @@ public class ServerTest {
         Main.main(new String[]{"-r", "testroot", "-x"});
         String output = outContent.toString();
         String expected = "Serving files from: " + new File("testroot").getAbsolutePath();
-        assertTrue(output.contains(expected));
+        Assertions.assertTrue(output.contains(expected));
     }
+
+    @Test
+    public void pFlagWithoutNumberPrintsInvalidPort(){
+        String expected = "Invalid port";
+        Main.main(new String[]{"-p", "*"});
+        String output = outContent.toString();
+        Assertions.assertTrue(output.contains(expected));
+    }
+
+    @Test
+    public void pFlagWithDecimalNumberPrintsPortRange(){
+        String expected = "Invalid port";
+        Main.main(new String[]{"-p", "8.6"});
+        Assertions.assertTrue(outContent.toString().contains(expected));
+    }
+
+    @Test
+    public void pFlagWithoutArgumentPrintsUsage(){
+        String expected = "-p     Specify the port.  Default is 80.";
+        Main.main(new String[]{"-p"});
+        String output = outContent.toString();
+        Assertions.assertTrue(output.contains(expected));
+    }
+
+    @Test
+    public void pFlagWithAnOccupiedPortReportsPortIsInUse() throws IOException {
+        String expected = "Port already in use:";
+        try(ServerSocket socket = new ServerSocket(0)){
+            int occupiedPort = socket.getLocalPort();
+            Main.main(new String[]{"-p", String.valueOf(occupiedPort)});
+            String output = outContent.toString();
+            assertTrue(output.contains(expected));
+        }
+    }
+
+    @Test
+    public void pFlagWithNegativeNumberPrintsPortRange(){
+        String expected = "Port must be between 1 and";
+        Main.main(new String[]{"-p", "-4"});
+        Assertions.assertTrue(outContent.toString().contains(expected));
+    }
+
+    @Test
+    public void rFlagWithoutArgumentPrintsUsage(){
+        String expected = "-r     Specify the root directory.  Default is the current working directory.";
+        Main.main(new String[]{"-r"});
+        Assertions.assertTrue(outContent.toString().contains(expected));
+    }
+
+    @Test
+    public void rFlagWithEmptyArgumentPrintsUsage(){
+        String expected = "-r     Specify the root directory.  Default is the current working directory.";
+        Main.main(new String[]{"-r", ""});
+        Assertions.assertTrue(outContent.toString().contains(expected));
+    }
+
+    @Test
+    public void rFlagWithInvalidRootPrintsDirectoryDoesNotExist(){
+        String expected = "Directory does not exist";
+        Main.main(new String[]{"-r", "junk"});
+        Assertions.assertTrue(outContent.toString().contains(expected));
+    }
+
+    @Test
+    public void rFlagWithInvalidRootPrintsInvalidDirectory(){
+        String expected = "Not a directory";
+        Main.main(new String[]{"-r", "pom.xml"});
+        Assertions.assertTrue(outContent.toString().contains(expected));
+    }
+
 }
